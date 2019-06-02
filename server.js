1d1468d0cb51aca1b25e71dfe81a287e51b4908b
Tre'Shawn <nbatre1@yahoo.com>
2019-06-02 16:03:40 -0500

    removing the first one

--- server.js
+++ server.js
@@ -18,18 +18,95 @@ var sys = require('util');
 
 
 
- app.get('/',(req,res,next)=>{
+//  app.get('/',(req,res,next)=>{
      
-    res.send('Hello world');
- })
+//     res.send('Hello world');
+//  })
 
- app.get('/test',(req,res,next) =>{
+//  app.post('/',(req,res,next) =>{
 
-    res.send('this is a fork of bitcoin');
- })
+//     res.send('Recieved a post http method');
+//  });
+
+//  app.put ('/:userId', (req,res,next) =>{
+
+//    res.send('put http method');
+//  })
 
 
+//  app.delete('/:userId',(req,res,next) =>{
 
+//    res.send('this is the delete post');
+//  })
+
+
+let users = {
+   1: {
+     id: '1',
+     username: 'Robin Wieruch',
+   },
+   2: {
+     id: '2',
+     username: 'Dave Davids',
+   },
+ };
+ 
+ let messages = {
+   1: {
+     id: '1',
+     text: 'Hello World',
+     userId: '1',
+   },
+   2: {
+     id: '2',
+     text: 'By World',
+     userId: '2',
+   },
+ };
+ 
+
+let message={};
+
+
+app.get('/users/',(req,res,next) => {
+
+   return res.send(Object.values(users))
+});
+
+app.get('/users/:userId', (req, res) => {
+   return res.send(users[req.params.userId]);
+ });
+
+
+app.get('/message',(req,res,next) =>{
+
+   return res.send(Object.values(messages));
+})
+
+app.get('/message/:messagesId',(req,res,next) =>{
+
+
+   return res.send(message[req.params.messagesIdd]);
+
+
+})
+
+ /////////////////////////////////////////////////////////
+
+app.post('/users' ,(req,res,next)=>{
+    res.send('Post http method for the users');
+})
+
+
+app.put('/users/:userId', (req,res) =>{
+
+   res.send(`you to select certain users ${req.params.userId}`)
+})
+
+
+app.delete('/users/:userId',(req,res) =>{
+   res.send(`you to select certain users ${req.params.userId}`)
+})
 
 let PORT = process.env.PORT || 8080;
 
